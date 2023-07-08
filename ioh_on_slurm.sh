220b5bd4cf830aa4d1136e2b5906c509882d4c52
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2023-07-08 20:44:16 +0200

    Update Slrum script

--- ioh_on_slurm.sh
+++ ioh_on_slurm.sh
@@ -6,9 +6,10 @@
 #SBATCH --error=Tmp/ma-bbob.sh.err
 #SBATCH --array=0-288%289
 #SBATCH --mem-per-cpu=3000
-#SBATCH --exclude=ethnode[01-06,08-09,11-20,24-28,30-32]
+#SBATCH --exclude=ethnode[01-06,08-13,15-20,22-28,30-32]
+#SBATCH --partition=graceADA
 ###
 params=( $(seq 0 288 ) )
 
 # To run the MA-BBOB problems for NGOpt choices:
-srun -N1 -n1 --mem-per-cpu=3000 python3 ioh_ng_real.py --pbs-index-bud-dep ${params[$SLURM_ARRAY_TASK_ID]}
+srun -N1 -n1 --mem-per-cpu=3000 python3 ioh_ng_real.py --pbs-index-bud-dep ${params[$SLURM_ARRAY_TASK_ID]} 1> test.out 2> test.err
