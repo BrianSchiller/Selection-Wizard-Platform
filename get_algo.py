a22626b73fd1b3fa38a8c56fe2421ca2b9e45f7b
Koen van der Blom <koen.vdblom@lip6.fr>
2024-05-07 17:37:38 +0200

    Start cleanup and documentation of get_algo.py

--- get_algo.py
+++ get_algo.py
@@ -3,6 +3,7 @@
 
 from enum import Enum
 from typing import Union
+from pathlib import Path
 
 import nevergrad as ng
 from nevergrad.optimization.base import Optimizer
@@ -25,56 +26,45 @@ class NGOptVersion(str, Enum):
     NGOpt39 = "NGOpt39"
 
 
-def get_optimiser(params: Array, budget: int, workers: int,
+def get_optimiser(params: Array,
+                  eval_budget: int,
+                  n_workers: int,
+                  n_dimensions: int,
                   ngopt: NGOptVersion) -> NGOpt:
     """Return an optimiser based on the properties and NGOpt version."""
     if ngopt == NGOptVersion.NGOpt39:
         optimiser = ng.optimizers.NGOpt39(
-            parametrization=(
-                ng.p.Array(shape=(1, n_dimensions)).set_bounds(
-                    const.LOWER_BOUND, const.UPPER_BOUND)),
+            parametrization=params,
             budget=eval_budget,
             num_workers=n_workers)
     elif ngopt == NGOptVersion.NGOpt36:
         optimiser = ng.optimizers.NGOpt36(
-            parametrization=(
-                ng.p.Array(shape=(1, n_dimensions)).set_bounds(
-                    const.LOWER_BOUND, const.UPPER_BOUND)),
+            parametrization=params,
             budget=eval_budget,
             num_workers=n_workers)
     elif ngopt == NGOptVersion.NGOpt21:
         optimiser = ng.optimizers.NGOpt21(
-            parametrization=(
-                ng.p.Array(shape=(1, n_dimensions)).set_bounds(
-                    const.LOWER_BOUND, const.UPPER_BOUND)),
+            parametrization=params,
             budget=eval_budget,
             num_workers=n_workers)
     elif ngopt == NGOptVersion.NGOpt16:
         optimiser = ng.optimizers.NGOpt16(
-            parametrization=(
-                ng.p.Array(shape=(1, n_dimensions)).set_bounds(
-                    const.LOWER_BOUND, const.UPPER_BOUND)),
+            parametrization=params,
             budget=eval_budget,
             num_workers=n_workers)
     elif ngopt == NGOptVersion.NGOpt15:
         optimiser = ng.optimizers.NGOpt15(
-            parametrization=(
-                ng.p.Array(shape=(1, n_dimensions)).set_bounds(
-                    const.LOWER_BOUND, const.UPPER_BOUND)),
+            parametrization=params,
             budget=eval_budget,
             num_workers=n_workers)
     elif ngopt == NGOptVersion.NGOpt14:
         optimiser = ng.optimizers.NGOpt14(
-            parametrization=(
-                ng.p.Array(shape=(1, n_dimensions)).set_bounds(
-                    const.LOWER_BOUND, const.UPPER_BOUND)),
+            parametrization=params,
             budget=eval_budget,
             num_workers=n_workers)
     elif ngopt == NGOptVersion.NGOpt8:
         optimiser = ng.optimizers.NGOpt8(
-            parametrization=(
-                ng.p.Array(shape=(1, n_dimensions)).set_bounds(
-                    const.LOWER_BOUND, const.UPPER_BOUND)),
+            parametrization=params,
             budget=eval_budget,
             num_workers=n_workers)
 
@@ -89,74 +79,123 @@ def short_name(algorithm: Optimizer) -> Union[Optimizer, str]:
     return algorithm
 
 
-def get_algorithm_for_ngopt(algorithm: Optimizer) -> Optimizer:
+def get_algorithm_for_ngopt(algorithm: Optimizer,
+                            params: Array,
+                            eval_budget: int,
+                            n_workers: int,
+                            n_dimensions: int) -> Optimizer:
     """Get the recommended algorithm if it is currently an NGOpt version."""
     # For some reason type() does not work on NGOpt classes
     if algorithm is ng.optimization.optimizerlib.NGOpt36:
         optimiser = get_optimiser(
-            params, eval_budget, n_workers, NGOptVersion.NGOpt36)
+            params, eval_budget, n_workers, n_dimensions, NGOptVersion.NGOpt36)
         algorithm = optimiser._select_optimizer_cls()
 
     if algorithm is ng.optimization.optimizerlib.NGOpt21:
         optimiser = get_optimiser(
-            params, eval_budget, n_workers, NGOptVersion.NGOpt21)
+            params, eval_budget, n_workers, n_dimensions, NGOptVersion.NGOpt21)
         algorithm = optimiser._select_optimizer_cls()
 
     if algorithm is ng.optimization.optimizerlib.NGOpt16:
         optimiser = get_optimiser(
-            params, eval_budget, n_workers, NGOptVersion.NGOpt16)
+            params, eval_budget, n_workers, n_dimensions, NGOptVersion.NGOpt16)
         algorithm = optimiser._select_optimizer_cls()
 
     if algorithm is ng.optimization.optimizerlib.NGOpt15:
         optimiser = get_optimiser(
-            params, eval_budget, n_workers, NGOptVersion.NGOpt15)
+            params, eval_budget, n_workers, n_dimensions, NGOptVersion.NGOpt15)
         algorithm = optimiser._select_optimizer_cls()
 
     if algorithm is ng.optimization.optimizerlib.NGOpt8:
         optimiser = get_optimiser(
-            params, eval_budget, n_workers, NGOptVersion.NGOpt8)
+            params, eval_budget, n_workers, n_dimensions, NGOptVersion.NGOpt8)
         algorithm = optimiser._select_optimizer_cls()
 
     return algorithm
 
 
-# Tracking variables
-latest_algortihm = ""
+def output_ngopt_algos(output_properties: bool,
+                       output_header: bool,
+                       hsv_format: bool,
+                       full_algo_name: bool,
+                       out_file: None) -> None:
+    """Print NGOpt algorithms to standard output or file."""
+    # Tracking variables
+    latest_algortihm = ""
 
-# Fixed properties
-n_workers = 1
+    # Fixed properties
+    n_workers = 1
 
-# Properties to be varied
-n_dims_min = 1
-n_dims_max = 100
-eval_budget_min = 1
-eval_budget_max = const.EVAL_BUDGET
+    # Properties to be varied
+    n_dims_min = 1
+    n_dims_max = 100
+    eval_budget_min = 1
+    eval_budget_max = const.EVAL_BUDGET
 
-# Print derived problem properties
-optimiser = ng.optimizers.NGOpt39(
-    parametrization=ng.p.Array(shape=(1, n_dims_min)).set_bounds(
-        const.LOWER_BOUND, const.UPPER_BOUND),
-    budget=eval_budget_min,
-    num_workers=n_workers)
-print("has_noise", optimiser.has_noise)
-print("fully_continuous", optimiser.fully_continuous)
-print("fully_bounded", ng.parametrization.parameter.helpers.Normalizer(
-    optimiser.parametrization).fully_bounded)
+    # Output derived problem properties
+    if output_properties:
+        optimiser = ng.optimizers.NGOpt39(
+            parametrization=ng.p.Array(shape=(1, n_dims_min)).set_bounds(
+                const.LOWER_BOUND, const.UPPER_BOUND),
+            budget=eval_budget_min,
+            num_workers=n_workers)
+        print("has_noise", optimiser.has_noise, file=out_file)
+        print("fully_continuous", optimiser.fully_continuous, file=out_file)
+        print("fully_bounded", ng.parametrization.parameter.helpers.Normalizer(
+            optimiser.parametrization).fully_bounded, file=out_file)
 
-# print("Algorithm, dimensionality, evaluation budget")
-print("Algorithm#dimensionality#evaluation budget")
+    # Output header
+    if output_header:
+        if hsv_format:
+            print("Algorithm#dimensionality#evaluation budget", file=out_file)
+        else:
+            print("Algorithm, dimensionality, evaluation budget",
+                  file=out_file)
 
-for n_dimensions in range(n_dims_min, n_dims_max + 1):
-    for eval_budget in range(eval_budget_min, eval_budget_max + 1):
+    for n_dimensions in range(n_dims_min, n_dims_max + 1):
         params = ng.p.Array(shape=(1, n_dimensions)).set_bounds(
             const.LOWER_BOUND, const.UPPER_BOUND)
-        optimiser = get_optimiser(
-            params, eval_budget, n_workers, NGOptVersion.NGOpt39)
-        algorithm = optimiser._select_optimizer_cls()
-        algorithm = get_algorithm_for_ngopt(algorithm)
-#        algorithm = short_name(algorithm)
 
-        if str(algorithm) != latest_algortihm:
-            # print(algorithm, n_dimensions, eval_budget)
-            print(f"{algorithm}#{n_dimensions}#{eval_budget}")
-            latest_algortihm = str(algorithm)
+        for eval_budget in range(eval_budget_min, eval_budget_max + 1):
+            optimiser = get_optimiser(
+                params, eval_budget, n_workers, n_dimensions,
+                NGOptVersion.NGOpt39)
+            algorithm = optimiser._select_optimizer_cls()
+            algorithm = get_algorithm_for_ngopt(algorithm, params, eval_budget,
+                                                n_workers, n_dimensions)
+
+            if not full_algo_name:
+                algorithm = short_name(algorithm)
+
+            if str(algorithm) != latest_algortihm:
+                if hsv_format:
+                    print(f"{algorithm}#{n_dimensions}#{eval_budget}",
+                          file=out_file)
+                else:
+                    print(algorithm, n_dimensions, eval_budget, file=out_file)
+
+                latest_algortihm = str(algorithm)
+    return
+
+
+# Output settings
+output_properties = True
+output_header = False
+hsv_format = False
+full_algo_name = False
+
+# Don't distinguish between different ConfPortfolio's (use short names)
+out_path = Path("ngopt_choices/0.6.0/dims1-100evals1-10000.txt")
+
+with out_path.open("w") as out_file:
+    output_ngopt_algos(output_properties, output_header, hsv_format,
+                       full_algo_name, out_file)
+
+
+# Distinguish between different ConfPortfolio's (use full names)
+full_algo_name = True
+out_path = Path("ngopt_choices/0.6.0/dims1-100evals1-10000_full.txt")
+
+with out_path.open("w") as out_file:
+    output_ngopt_algos(output_properties, output_header, hsv_format,
+                       full_algo_name, out_file)
